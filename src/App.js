be71fdba7592845eba6cc0282b4c3f0b5a3b2ddc
AndreaVanElmpt <oibibio@gmail.com>
2022-05-29 13:15:47 +0200

    bijna klaar

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
+import React from 'react';
+import logo from './assets/logo2.png';
+import PokeList from "./components/PokeList/PokeList";
 import './App.css';
 
+
+
 function App() {
-    const [pokemonData, setPokemonData] = useState({});
-
-
-    useEffect(()=>{ async function fetchData() {
-        try {
-            const result = await
-                axios.get(`https://pokeapi.co/api/v2/pokemon/jigglypuff`);
-            console.log(result.data);
-            setPokemonData(result.data)
-        } catch (e) {
-            console.error(e);
-        }
-    } fetchData()}, [])
 
   return (
  <>
-    <div>
-      Begin hier met de opdracht!
+     <div >
+         <img className='logo' src={logo} alt='Pokemon-logo' />
+     </div>
+        <PokeList />
 
-      <span>
-             {Object.keys(pokemonData).length > 0 &&
-                 <>
-                     <h2>{pokemonData.name}</h2>
-                     <h3><img src={pokemonData.sprites.front_default } alt={pokemonData.name}/></h3>
-                     <h3>Moves: {pokemonData.moves.length}</h3>
-                     <h3>Weight: {pokemonData.weight}</h3>
-                     <h3>Abilities:</h3>
-                     <ul>
-                         {pokemonData.abilities.map((ability, index) =>{
-                             return <li key={index}>{ability.ability.name}</li>
-                         })}
-                     </ul>
 
-                 </>
-             }
-      </span>
-    </div>
+
  </>
   );
 }
---
+++ src/components/PokeList/PokeList.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import React, {useEffect, useState} from "react";
+import PokeCard from "../PokeCard/PokeCard";
+import Button from "../Button/Button";
+import "./PokeList.css"
+
+function PokeList({}) {
+
+    const [pokemonData, setPokemonData] = useState([]);
+    const [endpoint, setEndpoint] = useState(`https://pokeapi.co/api/v2/pokemon/`)
+
+    useEffect(() => {
+    async function fetchData() {
+        try {
+            const result = await
+                axios.get(`${endpoint}`);
+                console.log(result.data)
+                setPokemonData(result.data);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+
+        fetchData();
+    }, [endpoint]);
+
+
+    return (
+
+     <>
+
+       <div className='buttons-control'>
+          <div className='botton-next-prev'>
+            <Button
+               type='button'
+               disabled={!pokemonData.previous}
+               onClickBtn={() => setEndpoint(pokemonData.previous)}
+            >
+             vorige
+           </Button>
+           <Button
+             type='button'
+             disabledBtn={!pokemonData.next}
+             onClickBtn={() => setEndpoint(pokemonData.next)}
+            >
+             volgende
+           </Button>
+          </div>
+
+       </div>
+
+              <div className='card-list'>
+                 {pokemonData.results && pokemonData.results.map((pokemon) => {
+                     return <PokeCard url={pokemon.url}  key={pokemon.name}/>
+                 })}
+             </div>
+         }
+
+
+     </>
+  );
+}
+export default PokeList;
